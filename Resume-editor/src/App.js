bf2e06205dae1de858aace52668957cefbd3029b
jambi997 <65461882+jambi997@users.noreply.github.com>
2024-01-10 01:00:41 +0100

    feat: project created

---
+++ Resume-editor/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ResumeEditor from './ResumeEditor';
+
+function App() {
+  return (
+    <div className="bg-green-900 h">
+      <ResumeEditor />
+    </div>
+  );
+}
+
+export default App;
---
+++ Resume-editor/src/ResumeEditor.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ResumeEditor = () => {
+  return (
+    <div>
+      ResumeEditor
+      <h1 class="text-3xl font-bold underline text-right">
+        Hello world!
+        </h1>
+    </div>
+  );
+};
+
+export default ResumeEditor;
